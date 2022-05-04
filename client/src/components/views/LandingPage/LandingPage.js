673ad4017dc632cd3bb11f471314669ca19cb7f9
hamsoter <omilzmil0w0@gmail.com>
2022-05-05 06:52:13 +0900

    Feat: DB에서 모든 꿈 상품 정보를 받아옴

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import Axios from "axios";
+import React, { useEffect } from "react";
 import { FaCode } from "react-icons/fa";
 
 function LandingPage() {
-    return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
-    )
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await Axios.post("/api/product/products");
+        console.log(result.data.products);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  });
+
+  return (
+    <main>
+      <></>
+    </main>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
