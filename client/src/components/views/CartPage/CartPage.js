d645d2e7bc1bc4f07faff3d3d049c83c75252c42
hamsoter <omilzmil0w0@gmail.com>
2022-05-09 11:26:27 +0900

    Feat: 장바구니 페이지 Route 연결

---
+++ client/src/components/views/CartPage/CartPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CartPage() {
+  return <main>CartPage</main>;
+}
+
+export default CartPage;
--- client/src/components/views/NavBar/Sections/RightMenu.js
+++ client/src/components/views/NavBar/Sections/RightMenu.js
@@ -1,10 +1,11 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import { Menu } from "antd";
+import { Avatar, Badge, Menu } from "antd";
 import axios from "axios";
 import { USER_SERVER } from "../../../Config";
 import { withRouter } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { ShoppingCartOutlined } from "@ant-design/icons";
 
 function RightMenu(props) {
   const user = useSelector((state) => state.user);
@@ -23,7 +24,7 @@ function RightMenu(props) {
     return (
       <Menu mode={props.mode}>
         <Menu.Item key="mail">
-          <a href="/login">꿈나라로 입장</a>
+          <a href="/login">로그인</a>
         </Menu.Item>
         <Menu.Item key="app">
           <a href="/register">가입</a>
@@ -33,11 +34,26 @@ function RightMenu(props) {
   } else {
     return (
       <Menu mode={props.mode}>
-        <Menu.Item key="logout">
-          <a onClick={logoutHandler}>나가기</a>
+        <Menu.Item key="cart" style={{ paddingRight: "20px" }}>
+          <Badge count={5}>
+            <a
+              href="/user/cart"
+              style={{ marginRight: "-20px", color: "#66777" }}
+            >
+              <ShoppingCartOutlined
+                style={{
+                  fontSize: "20px",
+                  marginBottom: "24px",
+                }}
+              />
+            </a>
+          </Badge>
         </Menu.Item>
         <Menu.Item key="upload">
           <a href="/product/upload">꿈 등록</a>
+        </Menu.Item>
+        <Menu.Item key="logout">
+          <a onClick={logoutHandler}>로그아웃</a>
         </Menu.Item>
       </Menu>
     );
